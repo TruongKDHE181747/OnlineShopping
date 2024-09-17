88f10c816780fa595f5e1806cbdff6657f3e29da
TruongKDHE181747 <truongkdhe181747@fpt.edu.vn>
2024-09-17 15:46:47 +0700

    Add folder brand_img, product_img folder

--- src/java/slider_controller/SliderDemo.java
+++ src/java/slider_controller/SliderDemo.java
@@ -3,20 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
-package controller;
+package slider_controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author quanpyke
+ * @author Dell
  */
-public class sliderlist extends HttpServlet {
+@WebServlet(name="SliderDemo", urlPatterns={"/sliderdemo"})
+public class SliderDemo extends HttpServlet {
    
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
@@ -33,10 +35,10 @@ public class sliderlist extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet sliderlist</title>");  
+            out.println("<title>Servlet SliderDemo</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet sliderlist at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet SliderDemo at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
