550be733a73dd1e1327012dc09522b33469a1143
DucDMHE181735 <ducdmhe181735@fpt.edu.vn>
2024-09-18 21:16:02 +0700

    -Add Constants.java, Email.java -Update UserDAO, Role, User file

---
+++ src/java/utils/Constants.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+public class Constants {
+    public static final String SENT_EMAIL = "ducdmhe181735@fpt.edu.vn" ;
+    public static final String EMAIL_PASSWORD = "afux tpky avpr vqpa" ;
+    
+}
