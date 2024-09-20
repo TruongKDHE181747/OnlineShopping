0bba6bbdd3b84336bb38c5832f1b0acc14749242
DucDMHE181735 <ducdmhe181735@fpt.edu.vn>
2024-09-20 14:07:16 +0700

    -Add servlet login, register, verify and verify.jsp -Update UserDAO.java, Email.java, login.jsp, register.jsp

---
+++ src/java/auth_controller/Verify.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package auth_controller;
+
+import dal.UserDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import model.User;
+import utils.Email;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "Verify", urlPatterns = {"/verify"})
+public class Verify extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        
+        Email mail = new Email();
+        
+        User user = (User) session.getAttribute("registeredAccount");
+        
+        if(user == null){
+            return;
+        }
+        //send mail to user to activate account
+        boolean checkSendMail = mail.sendRegisterEmail(user);
+
+//        fail to send email
+        if (!checkSendMail) {
+            String errorMessage = "Cannot sent email!";
+            request.setAttribute("error", errorMessage);
+            request.getRequestDispatcher("/account/register.jsp").forward(request, response);
+            return;
+        }
+
+        request.getRequestDispatcher("/account/verify.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(true);
+
+        UserDAO userDAO = new UserDAO();
+
+        String otp = request.getParameter("otp");
+
+        User user = (User) session.getAttribute("registeredAccount");
+
+        if(user == null){
+            response.sendRedirect(request.getContextPath()+"/register");
+            return;
+        }
+        if (!otp.equals(user.getVerification_code())) {
+            String errorMessage = "Wrong verification code!";
+            request.setAttribute("error", errorMessage);
+            request.getRequestDispatcher("/account/verify.jsp").forward(request, response);
+            return;
+        }
+
+        boolean check = userDAO.activateUser(user.getUsername());
+        
+        if(!check){
+            response.sendRedirect(request.getContextPath()+"/login");
+        }
+
+        //response.sendRedirect(request.getContextPath()+"/login");
+
+    }
+
+}
