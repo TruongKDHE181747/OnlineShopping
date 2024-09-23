26c630db1934cd6ccd211cf64b0361234aab460c
TruongKDHE181747 <truongkdhe181747@fpt.edu.vn>
2024-09-23 23:48:44 +0700

    Add PostPagination.java Edit HPostList.java, post.jsp

--- src/java/post_controller/PostPagination.java
+++ src/java/post_controller/PostPagination.java
@@ -5,7 +5,6 @@
 
 package post_controller;
 
-import dal.PostDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -17,14 +16,13 @@ import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 import model.Post;
-import model.ProductFeedback;
 
 /**
  *
  * @author Dell
  */
-@WebServlet(name="HPostList", urlPatterns={"/hpostlist"})
-public class HPostList extends HttpServlet {
+@WebServlet(name="PostPagination", urlPatterns={"/postpagination"})
+public class PostPagination extends HttpServlet {
    
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
@@ -37,26 +35,18 @@ public class HPostList extends HttpServlet {
     throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession session = request.getSession();
-        PostDAO pdao = new PostDAO();
-        List<Post> pList = pdao.getAllPost();
-        List<Post> top6post = select6Post(pList, 0);
+        List<Post> allpostlist = (List<Post>)session.getAttribute("allpostlist");
+        int cpage = Integer.parseInt(request.getParameter("cpage"));
+        List<Post> top6post = select6Post(allpostlist, cpage);
         
-        session.setAttribute("allpostlist", pList);
+        
+        
         session.setAttribute("top6post", top6post);
-        session.setAttribute("cpostpage", 0);
+        session.setAttribute("cpostpage", cpage);
         
         
         response.sendRedirect(request.getContextPath()+"/common/post.jsp");
-        
-//        List<Post> top6Post1 = (List<Post>)session.getAttribute("top6Post");
-//        List<Post> allpostlist = (List<Post>)session.getAttribute("allpostlist");
-//        
-//        for (Post post : top6Post1) {
-//            
-//        }
-
     } 
-    
     
     public static List<Post> select6Post( List<Post> pList, int pageNum){
         List<Post> top6List = new ArrayList<>();
