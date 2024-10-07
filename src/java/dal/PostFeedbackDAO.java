45ea2249a0df4af2ff83ad188379e8dad6a0fc11
quannthe181061 <quannthe181061@fpt.edu.vn>
2024-10-07 14:35:13 +0700

    Fix css for search Sliders Add postfeedbacklist.jsp

--- src/java/dal/PostFeedbackDAO.java
+++ src/java/dal/PostFeedbackDAO.java
@@ -95,4 +95,40 @@ public class PostFeedbackDAO extends DBContext{
         }
         
     }
+     
+       public List<PostFeedback> getAllFeedback (int page) {
+        List<PostFeedback> pList = new ArrayList<>();
+       String sql = "select pf.*, u.username, u.profile_picture_url from Post_Feedbacks as pf, Users as u\n"
+               + "where pf.customer_id = u.user_id and pf.is_active=1 and u.is_banned=0"
+               + "   order by post_id\n"
+               + " offset ? rows\n"
+               + " fetch first 5 rows only ";
+           PostDAO pdao=new  PostDAO();
+        try {
+            PreparedStatement pre = connection.prepareStatement(sql);
+            pre.setInt(1,(page-1)*5);
+            ResultSet rs = pre.executeQuery();
+            while (rs.next()) {
+                int post_feedback_id = rs.getInt("post_feedback_id");
+                int customer_id = rs.getInt("customer_id");
+                int post_id = rs.getInt("post_id"); 
+                String review = rs.getString("review");
+                int is_active = rs.getInt("is_active");
+                String username = rs.getString("username");
+                String profile_picture_url = rs.getString("profile_picture_url");
+                Post post= pdao.getPostByID(String.valueOf(post_id));
+                PostFeedback pf= new PostFeedback(post_feedback_id, customer_id, review, is_active, username, profile_picture_url, post);
+                
+                
+                pList.add(pf);
+                
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(RoleDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return pList;
+    }
+     
+     
+     
 }
